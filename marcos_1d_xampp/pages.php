c50e453e83c3f6acebecd84885179dcb06ccf643
marcosferreira715 <marcos.arauj@escola.pr.gov.br>
2025-09-16 12:11:01 -0300

    Add files via upload

---
+++ marcos_1d_xampp/pages.php
@@ -0,0 +1,38 @@
+<?php
+include "conexao.php"; 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <form>
+    <style>
+        body {
+    font-family: 'Roboto', sans-serif;
+
+        }
+      input {
+            color: blue;
+        }
+
+    </style>
+<div style="text-align: center;">
+<h1>Area de Login</h1>
+<input type="text">
+<input type="password">
+<input type="email">
+<button type="submit">
+ Enviar
+</button>
+</div>
+
+</form>
+
+</body>
+</html>
